ae1d94adfe0f0fb68d9f9730e06c7678ea04ddde
Berkan Serbes <69859539+berkanserbes@users.noreply.github.com>
2025-02-01 20:44:30 +0300

    Time: 591 ms (87.86%), Space: 0B (100%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+SELECT DISTINCT num AS ConsecutiveNums
+FROM Logs l1
+WHERE 
+EXISTS(SELECT * FROM Logs l2 WHERE l2.id = l1.id - 1 AND l1.num = l2.num)
+AND
+EXISTS(SELECT * FROM Logs l3 WHERE l3.id = l1.id - 2 AND l3.num = l1.num)
+
